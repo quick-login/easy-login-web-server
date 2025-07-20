5d62b6ad627fe23633fe69c434ff72313c60f741
송광호 <skh990427@naver.com>
2025-07-20 19:16:53 +0900

    메인웹 서버 프로젝트 생성

---
+++ src/main/java/kr/co/easylogin/easyloginwebserver/EasyloginWebServerApplication.java
@@ -0,0 +1,13 @@
+package kr.co.easylogin.easyloginwebserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyloginWebServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyloginWebServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/easylogin/easyloginwebserver/EasyloginWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.easylogin.easyloginwebserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyloginWebServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
