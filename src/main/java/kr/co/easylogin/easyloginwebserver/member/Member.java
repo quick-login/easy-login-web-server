dfb3e744ba5d2afb3d99ace229dda441e3233c02
송광호 <skh990427@naver.com>
2025-07-29 22:23:34 +0900

    feat :: 회원 엔티티 매핑

---
+++ src/main/java/kr/co/easylogin/easyloginwebserver/member/Member.java
@@ -0,0 +1,45 @@
+package kr.co.easylogin.easyloginwebserver.member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import kr.co.easylogin.easyloginwebserver.common.BaseEntity;
+import kr.co.easylogin.easyloginwebserver.member.value.MemberStatus;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Entity
+@Slf4j
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(nullable = false, length = 60)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String role;
+
+    @Column(nullable = false)
+    private Long remainCount;
+
+    @Column(nullable = false)
+    private Long maxKakaoAppCount;
+
+    @Column(length = 20)
+    private String kakaoId;
+
+    @Column(nullable = false)
+    private Long cash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private MemberStatus status;
+}
