5b8aa1fad88f749ad522ab1eb5cb5de9505e209e
송광호 <skh990427@naver.com>
2025-07-30 20:48:09 +0900

    feat :: 공통 응답포맷 추가

---
+++ src/main/java/kr/co/easylogin/easyloginwebserver/common/dto/ResponseDto.java
@@ -0,0 +1,29 @@
+package kr.co.easylogin.easyloginwebserver.common.dto;
+
+import kr.co.easylogin.easyloginwebserver.common.dto.value.ResponseCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class ResponseDto<T> {
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+    private final T data;
+
+    public ResponseDto(ResponseCode code, T data) {
+        this.status = code.getStatus();
+        this.code = code.getCode();
+        this.message = code.getMessage();
+        this.data = data;
+    }
+
+    public static <T> ResponseDto<T> of(ResponseCode code, T data) {
+        return new ResponseDto<>(code, data);
+    }
+
+    public static <T> ResponseDto<T> of(ResponseCode code) {
+        return new ResponseDto<>(code, null);
+    }
+}
