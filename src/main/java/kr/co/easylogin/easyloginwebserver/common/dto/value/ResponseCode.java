f32243d1e07972b368247a83f38068e429716543
송광호 <skh990427@naver.com>
2025-07-31 19:13:53 +0900

    refactor :: 공통응답포맷 일부 수정

--- src/main/java/kr/co/easylogin/easyloginwebserver/common/dto/value/ResponseCode.java
+++ src/main/java/kr/co/easylogin/easyloginwebserver/common/dto/value/ResponseCode.java
@@ -13,8 +13,13 @@ public enum ResponseCode {
 
     // 사용자 에러
     DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "U1000", "현재 가입되어있는 이메일입니다."),
-    PASSWORD_CHECK_ERROR(HttpStatus.BAD_REQUEST, "U1001", "비밀번호와 비밀번호 확인값이 일치하지 않습니다.");
+    PASSWORD_CHECK_ERROR(HttpStatus.BAD_REQUEST, "U1001", "비밀번호와 비밀번호 확인값이 일치하지 않습니다."),
 
+    // 사용자 입력 에러
+    INVALID_INPUT(HttpStatus.BAD_REQUEST, "E4000", "유효하지 않은 입력값입니다."),
+
+    // 서버 에러
+    SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "E5000", "정의되지않은 서버 에러");
 
     private final HttpStatus status;
     private final String code;
--- src/main/java/kr/co/easylogin/easyloginwebserver/common/error/ExceptionControllerAdvice.java
+++ src/main/java/kr/co/easylogin/easyloginwebserver/common/error/ExceptionControllerAdvice.java
@@ -1,20 +1,44 @@
 package kr.co.easylogin.easyloginwebserver.common.error;
 
+import jakarta.servlet.http.HttpServletResponse;
 import kr.co.easylogin.easyloginwebserver.common.dto.ResponseDto;
+import kr.co.easylogin.easyloginwebserver.common.dto.value.ResponseCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 @RequiredArgsConstructor
 public class ExceptionControllerAdvice {
 
+    private final HttpServletResponse response;
+
     @ExceptionHandler(value = BusinessException.class)
-    public ResponseEntity<ResponseDto<Object>> handleBusinessException(BusinessException exception) {
-        ResponseDto<Object> responseDto = ResponseDto.of(exception.getResponseCode());
-        return new ResponseEntity<>(responseDto, responseDto.getStatus());
+    public ResponseDto<Object> handleBusinessException(BusinessException exception) {
+        response.setStatus(exception.getResponseCode().getStatus().value());
+        return ResponseDto.of(exception.getResponseCode());
+    }
+
+    /**
+     * 지원하지 않는 API 요청시
+     */
+    @ExceptionHandler(value = NoResourceFoundException.class)
+    public ResponseDto<Object> handleBusinessException(NoResourceFoundException exception) {
+        response.setStatus(ResponseCode.INVALID_INPUT.getStatus().value());
+        log.error("지원하지 않는 URI 요청 : {}", exception.getMessage());
+        return ResponseDto.of(ResponseCode.INVALID_INPUT);
+    }
+
+    /**
+     * 정의되지 않은 서버 오류
+     */
+    @ExceptionHandler(value = Exception.class)
+    public ResponseDto<Object> handleException(Exception exception) {
+        response.setStatus(ResponseCode.SERVER_ERROR.getStatus().value());
+        log.error("정의되지않은 서버 오류 : {}", exception.toString());
+        return ResponseDto.of(ResponseCode.SERVER_ERROR);
     }
 }
