99ad8584078ff433377f89039196c4d92a4fd8df
송광호 <skh990427@naver.com>
2025-08-06 19:15:49 +0900

    feat :: 공통응답 어드바이스 추가

--- src/main/java/kr/co/easylogin/easyloginwebserver/member/MemberController.java
+++ src/main/java/kr/co/easylogin/easyloginwebserver/member/MemberController.java
@@ -1,11 +1,9 @@
 package kr.co.easylogin.easyloginwebserver.member;
 
 import jakarta.validation.Valid;
-import kr.co.easylogin.easyloginwebserver.common.dto.ResponseDto;
 import kr.co.easylogin.easyloginwebserver.member.dto.request.SignupRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +18,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signup")
-    public ResponseEntity<ResponseDto<Object>> signup(@Valid @RequestBody SignupRequest signupRequest) {
-        ResponseDto<Object> result = memberService.signup(signupRequest);
-        return new ResponseEntity<>(result, result.getStatus());
+    public void signup(@Valid @RequestBody SignupRequest signupRequest) {
+        memberService.signup(signupRequest);
     }
 }
