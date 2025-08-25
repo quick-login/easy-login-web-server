71301febe1da2aad780a30bf58d42e59085eae09
송광호 <skh990427@naver.com>
2025-08-25 19:48:23 +0900

    feat :: 문의사항 엔티티 매핑

---
+++ src/main/java/kr/co/easylogin/easyloginwebserver/question/domain/Question.java
@@ -0,0 +1,30 @@
+package kr.co.easylogin.easyloginwebserver.question.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import kr.co.easylogin.easyloginwebserver.common.BaseEntity;
+import kr.co.easylogin.easyloginwebserver.member.Member;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Entity
+@Slf4j
+@Getter
+public class Question extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    @Column(nullable = false, length = 1000)
+    private String content;
+
+    @Column(length = 1000)
+    private String answer;
+}
