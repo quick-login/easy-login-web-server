c0aa4c9510dfd6679690f58eca2ff691715a1294
송광호 <skh990427@naver.com>
2025-09-15 23:16:00 +0900

    feat :: 주문관련 엔티티 추가

---
+++ src/main/java/kr/co/easylogin/easyloginwebserver/order/domain/OrderHistory.java
@@ -0,0 +1,31 @@
+package kr.co.easylogin.easyloginwebserver.order.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import kr.co.easylogin.easyloginwebserver.common.BaseEntity;
+import kr.co.easylogin.easyloginwebserver.product.domain.Product;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Entity
+@Slf4j
+@Getter
+public class OrderHistory extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_history_id")
+    private OrderHistory orderHistory;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(nullable = false)
+    private Long orderQuantity;
+
+    @Column(nullable = false)
+    private Long price;
+}
