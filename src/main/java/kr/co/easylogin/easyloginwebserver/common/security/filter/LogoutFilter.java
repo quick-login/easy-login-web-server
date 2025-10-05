ff6acb837c84e5607c6ccb60f76eecbce2ec7c3d
송광호 <skh990427@naver.com>
2025-10-06 03:05:39 +0900

    refactor :: 로그아웃필터 응답값 추가

--- src/main/java/kr/co/easylogin/easyloginwebserver/common/security/filter/LogoutFilter.java
+++ src/main/java/kr/co/easylogin/easyloginwebserver/common/security/filter/LogoutFilter.java
@@ -1,9 +1,13 @@
 package kr.co.easylogin.easyloginwebserver.common.security.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import kr.co.easylogin.easyloginwebserver.common.dto.ResponseDto;
+import kr.co.easylogin.easyloginwebserver.common.dto.value.ResponseCode;
 import kr.co.easylogin.easyloginwebserver.common.utils.RedisUtil;
 import kr.co.easylogin.easyloginwebserver.config.SecurityConfiguration;
 import lombok.RequiredArgsConstructor;
@@ -12,8 +16,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
-
 @Slf4j
 @RequiredArgsConstructor
 public class LogoutFilter extends OncePerRequestFilter {
@@ -21,7 +23,8 @@ public class LogoutFilter extends OncePerRequestFilter {
     private final RedisUtil redisUtil;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        throws ServletException, IOException {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
         if (auth != null && auth.isAuthenticated()) {
@@ -30,7 +33,9 @@ public class LogoutFilter extends OncePerRequestFilter {
         }
 
         response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().write("Logged out successfully");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String responseJson = objectMapper.writeValueAsString(ResponseDto.of(ResponseCode.SUCCESS));
+        response.getWriter().write(responseJson);
     }
 
     @Override
