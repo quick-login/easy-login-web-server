8eebc5b817aeaa3f8c808d1b61b7f7f4d54f5a08
송광호 <skh990427@naver.com>
2025-10-23 20:33:57 +0900

    refactor :: 회원정보 조회시 역할 반환

--- src/main/java/kr/co/easylogin/easyloginwebserver/member/dto/response/MemberInfoResponse.java
+++ src/main/java/kr/co/easylogin/easyloginwebserver/member/dto/response/MemberInfoResponse.java
@@ -1,10 +1,11 @@
 package kr.co.easylogin.easyloginwebserver.member.dto.response;
 
 import kr.co.easylogin.easyloginwebserver.member.Member;
+import kr.co.easylogin.easyloginwebserver.member.value.MemberRole;
 import lombok.Builder;
 
 @Builder
-public record MemberInfoResponse(String name, String email, Long cash, Long remainCount, Long maxKakaoAppCount) {
+public record MemberInfoResponse(String name, String email, Long cash, Long remainCount, Long maxKakaoAppCount, MemberRole role) {
 
     public static MemberInfoResponse of(Member member) {
         return MemberInfoResponse.builder()
@@ -13,6 +14,7 @@ public record MemberInfoResponse(String name, String email, Long cash, Long rema
                                  .cash(member.getCash())
                                  .remainCount(member.getRemainCount())
                                  .maxKakaoAppCount(member.getMaxKakaoAppCount())
+                                 .role(member.getRole())
                                  .build();
     }
 }
