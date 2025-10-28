ec2e1fcd2ef0b9873e4f461107005a331941e9ee
송광호 <skh990427@naver.com>
2025-10-28 23:07:16 +0900

    refactor :: 캐시 관련 내역조회 날짜 포맷팅 추가

--- src/main/java/kr/co/easylogin/easyloginwebserver/cash/dto/response/CashChargeDetailsInfoResponse.java
+++ src/main/java/kr/co/easylogin/easyloginwebserver/cash/dto/response/CashChargeDetailsInfoResponse.java
@@ -1,12 +1,12 @@
 package kr.co.easylogin.easyloginwebserver.cash.dto.response;
 
-import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import kr.co.easylogin.easyloginwebserver.cash.CashChargeLog;
 import kr.co.easylogin.easyloginwebserver.cash.value.CashChargeStatus;
 import lombok.Builder;
 
 @Builder
-public record CashChargeDetailsInfoResponse(Long cashChargeLogId, Long chargeCash, CashChargeStatus status, LocalDateTime requestDate,
+public record CashChargeDetailsInfoResponse(Long cashChargeLogId, Long chargeCash, CashChargeStatus status, String requestDate,
                                             Long memberId, String name) {
 
     public static CashChargeDetailsInfoResponse of(CashChargeLog cashChargeLog) {
@@ -14,7 +14,8 @@ public record CashChargeDetailsInfoResponse(Long cashChargeLogId, Long chargeCas
                                             .cashChargeLogId(cashChargeLog.getId())
                                             .chargeCash(cashChargeLog.getChargeCash())
                                             .status(cashChargeLog.getStatus())
-                                            .requestDate(cashChargeLog.getCreatedAt())
+                                            .requestDate(
+                                                cashChargeLog.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
                                             .memberId(cashChargeLog.getMember().getId())
                                             .name(cashChargeLog.getMember().getName())
                                             .build();
--- src/main/java/kr/co/easylogin/easyloginwebserver/cash/dto/response/CashChargeInfoResponse.java
+++ src/main/java/kr/co/easylogin/easyloginwebserver/cash/dto/response/CashChargeInfoResponse.java
@@ -1,19 +1,19 @@
 package kr.co.easylogin.easyloginwebserver.cash.dto.response;
 
-import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import kr.co.easylogin.easyloginwebserver.cash.CashChargeLog;
 import kr.co.easylogin.easyloginwebserver.cash.value.CashChargeStatus;
 import lombok.Builder;
 
 @Builder
-public record CashChargeInfoResponse(Long cashChargeLogId, Long chargeCash, CashChargeStatus status, LocalDateTime requestDate) {
+public record CashChargeInfoResponse(Long cashChargeLogId, Long chargeCash, CashChargeStatus status, String requestDate) {
 
     public static CashChargeInfoResponse of(CashChargeLog cashChargeLog) {
         return CashChargeInfoResponse.builder()
                                      .cashChargeLogId(cashChargeLog.getId())
                                      .chargeCash(cashChargeLog.getChargeCash())
                                      .status(cashChargeLog.getStatus())
-                                     .requestDate(cashChargeLog.getCreatedAt())
+                                     .requestDate(cashChargeLog.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
                                      .build();
     }
 }
