a0a7973069851815ccf052904bf60d333f866494
송광호 <skh990427@naver.com>
2025-11-01 22:26:00 +0900

    feat :: 주문 상세조회시 최종 할인가 적용가, 최종 합산가 표기

--- src/main/java/kr/co/easylogin/easyloginwebserver/order/dto/response/OrderDetailsResultResponse.java
+++ src/main/java/kr/co/easylogin/easyloginwebserver/order/dto/response/OrderDetailsResultResponse.java
@@ -10,15 +10,22 @@ public record OrderDetailsResultResponse(
     String orderCode,
     String orderDate,
     Long totalPrice,
+    Long totalFinalPrice,
     List<OrderDetailsProduct> orderProducts
 
 ) {
 
     public static OrderDetailsResultResponse of(OrderHistory history) {
+        List<OrderDetailsProduct> orderDetailsProducts = history.getOrderDetails().stream().map(OrderDetailsProduct::of).toList();
+        Long totalPrice = 0L;
+        for (OrderDetailsProduct orderDetailsProduct : orderDetailsProducts) {
+            totalPrice += orderDetailsProduct.price();
+        }
         return OrderDetailsResultResponse.builder()
                                          .orderCode(history.getOrderCode())
                                          .orderDate(history.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
-                                         .totalPrice(history.getTotalPrice())
+                                         .totalPrice(totalPrice)
+                                         .totalFinalPrice(history.getTotalPrice())
                                          .orderProducts(history.getOrderDetails().stream().map(OrderDetailsProduct::of).toList())
                                          .build();
     }
