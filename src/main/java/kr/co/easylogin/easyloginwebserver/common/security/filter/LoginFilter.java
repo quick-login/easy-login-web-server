af96657fda490e6863b171fc421c18de538abaf0
송광호 <skh990427@naver.com>
2025-12-25 19:43:19 +0900

    feat :: 아이피 로깅 추가 해보기.. 이상하게 누적됨

--- src/main/java/kr/co/easylogin/easyloginwebserver/common/security/filter/LoginFilter.java
+++ src/main/java/kr/co/easylogin/easyloginwebserver/common/security/filter/LoginFilter.java
@@ -69,22 +69,28 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
     private String getClientIP(HttpServletRequest request) {
         String ip = request.getHeader("X-Forwarded-For");
+        log.info("X-Forwarded-For : {}", ip);
 
         if (ip == null) {
             ip = request.getHeader("Proxy-Client-IP");
+            log.info("Proxy-Client-IP : {}", ip);
         }
         if (ip == null) {
             ip = request.getHeader("WL-Proxy-Client-IP");
+            log.info("WL-Proxy-Client-IP : {}", ip);
         }
         if (ip == null) {
             ip = request.getHeader("HTTP_CLIENT_IP");
+            log.info("HTTP_CLIENT_IP : {}", ip);
         }
         if (ip == null) {
             ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+            log.info("HTTP_X_FORWARDED_FOR : {}", ip);
         }
         if (ip == null) {
             // -Djava.net.preferIPv4Stack=true VM 옵션에 추가해야 IPv4 형태로 뽑아옴
             ip = request.getRemoteAddr();
+            log.info("request.getRemoteAddr() : {}", ip);
         }
 
         return ip;
